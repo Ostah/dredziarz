98919ee8c40f5618ca6cb8f828e528006f903245
Ostah <Ostah@Ostah-Komputer>
2013-01-05 19:04:40 +0100

    zadrozny2

--- src/java/Zadrozny.java
+++ src/java/Zadrozny.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -14,12 +15,37 @@ public class Zadrozny {
     
     public enum tNorm {
 		NORM1, NORM2
-	}
-      public enum sNorm {
+    }
+    
+    public enum sNorm {
 		NORM1, NORM2
-	}
-    Zadrozny(List<Krotka> list,tNorm t, sNorm s )
+    }
+    
+    static float compute(ArrayList<Krotka> list, int elementId, tNorm t, sNorm s)
     {
         
+        float internalMax = 0.0f;
+        float outerMax = 0.0f;
+        int position = -1;
+    
+        
+      
+        
+       
+        for (int i = 0; i < list.size(); i++) {
+
+            //znajduje krotkę dla której mam obliczyć stopień przynależności
+            if(list.get(i).id == elementId) position = i;
+            
+            //znajduje największą wartość z min(C(s),P(s))
+            float minimum = Math.min(list.get(i).cId, list.get(i).pId);
+            if (internalMax < minimum) internalMax = minimum;
+  
+        }
+       
+        assert position!= -1 : "Nie znaleziono elementu o tym id";
+
+        outerMax = Math.max(1-internalMax, list.get(elementId).pId);
+        return Math.min(list.get(elementId).cId, outerMax);
     }
 }
