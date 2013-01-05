95108ff2f9b4745bb8e9e2ff15df6f147db13bbc
Ostah <Ostah@Ostah-Komputer>
2013-01-05 18:27:43 +0100

    zadrozny

---
+++ src/java/Zadrozny.java
@@ -0,0 +1,25 @@
+
+import java.util.List;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ostah
+ */
+public class Zadrozny {
+    
+    public enum tNorm {
+		NORM1, NORM2
+	}
+      public enum sNorm {
+		NORM1, NORM2
+	}
+    Zadrozny(List<Krotka> list,tNorm t, sNorm s )
+    {
+        
+    }
+}
