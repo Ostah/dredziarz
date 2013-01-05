70b081263e4bfba3a520ca21e7a4bb9189f966db
Ostah <Ostah@Ostah-Komputer>
2013-01-05 20:47:44 +0100

    droplisty do norm

--- src/java/fuzzyLogic.java
+++ src/java/fuzzyLogic.java
@@ -123,6 +123,8 @@ public class fuzzyLogic extends HttpServlet {
         List<Rekord> lista = logika.start();
         
         request.setAttribute("resultList", lista);
+        request.setAttribute("minArea", Integer.parseInt(request.getParameter("minArea")));
+        request.setAttribute("maxArea", Integer.parseInt(request.getParameter("maxArea")));
         request.setAttribute("minCena", Integer.parseInt(request.getParameter("minBudget")));
         request.setAttribute("maxCena", Integer.parseInt(request.getParameter("maxBudget")));
         getServletContext().getRequestDispatcher("/showResultList.jsp").forward(request, response);
