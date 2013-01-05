97f5b3295f00b3e037cb65b23b4df55fcb1f3740
Jacek Lyp <jaceklyp@gmail.com>
2013-01-05 18:25:03 +0100

    Dodany model Krotka

---
+++ src/java/Krotka.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jacek
+ */
+public class Krotka 
+{
+    int id;
+    float cId;
+    float pId;
+    
+    Krotka (int id, float cId, float pId) 
+    {
+        
+    }
+}
